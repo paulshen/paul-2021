b04b5b3b092afe9018f0d8fa26b5d329010b3aa7
Paul Shen <paulshen0@gmail.com>
2020-12-10 22:13:47 -0800

    use Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: [
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -6,6 +8,9 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
