b867121f05c4df68e59bbbc17a79913773af56ec
Paul Shen <paulshen0@gmail.com>
2020-12-10 18:58:36 -0800

    use tailwind in react-notion

--- pages/my-post.tsx
+++ pages/my-post.tsx
@@ -1,20 +1,32 @@
 import { NotionRenderer } from "react-notion/src/index";
 
+const addDashesToUUID = (s: string) =>
+  `${s.substr(0, 8)}-${s.substr(8, 4)}-${s.substr(12, 4)}-${s.substr(
+    16,
+    4
+  )}-${s.substr(20)}`;
+
 export async function getStaticProps() {
+  const pageId = "56e1d1eabd4f42a2bb591134f742535a";
   const data = await fetch(
-    "https://notion-api.splitbee.io/v1/page/56e1d1eabd4f42a2bb591134f742535a"
+    `https://notion-api.splitbee.io/v1/page/${pageId}`
   ).then((res) => res.json());
 
   return {
     props: {
+      pageId,
       blockMap: data,
     },
   };
 }
 
-const Page = ({ blockMap }) => (
-  <div className="max-w-lg">
-    <NotionRenderer blockMap={blockMap} />
-  </div>
-);
+const Page = ({ pageId, blockMap }) => {
+  const pageTitle = blockMap[addDashesToUUID(pageId)].value.properties.title[0];
+  return (
+    <div className="max-w-xl pt-16 mx-auto">
+      <h1 className="text-3xl mb-8 font-bold">{pageTitle}</h1>
+      <NotionRenderer blockMap={blockMap} />
+    </div>
+  );
+};
 export default Page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./react-notion/src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
