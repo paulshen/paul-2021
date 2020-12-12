30422815e0ce4f76b656d9904e588d3b788a37a9
Paul Shen <paulshen0@gmail.com>
2020-12-12 14:34:01 -0800

    default page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Header from "../components/Header";
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="text-gray-900">
+      <Head>
+        <title>Paul Shen</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
